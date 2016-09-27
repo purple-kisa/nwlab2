a2926c830e5ef0c340874ae17388af53d3556836
Hazel Lau <Hazel Lau>
2016-09-27 12:39:33 +0800

    we are on to lab 3! updated with friendbook stuff :D

--- NWLab2.py
+++ NWLab2.py
@@ -3,16 +3,22 @@
 # Hazel Lau (1001150)
 # Samuel Lim (1000971) 
 
-
 # coding: utf-8
 
 # In[ ]:
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, Response, json
 from random import randint
 import requests
 
 app = Flask(__name__) 
+
+fb = {}
+fb['Kisa'] = ['55 05-31', '+6591234567', 'kisa@meow.com', '6th February']
+fb['Samsam'] = ['55 08-35', '+6591234567', 'samsam@meow.com', '25th January']
+fb['Shonz'] = ['59 09-smth', '+6591234567', 'shonz@meow.com', '26th October']
+fb['Js'] = ['Punggol', '+6591234567', 'js@meow.com', '26th January']
+fb['Junqi'] = ['Bedok', '+6591234567', 'junqi@meow.com', 'March']
 
 @app.route('/')
 def index(): 
@@ -21,6 +27,29 @@ def index():
         return render_template('stuffsaid.html', stuffsaid=stuffsaid)
     else:
         return render_template('htmltemplate.html')
+
+@app.route('/friendbook')
+def friendbook(): 
+    return render_template('friendbook.html', fb = fb)
+
+@app.route('/friendbook/<name>')
+def api_name(name):
+    param = fb.get(name, None)
+    if param!=None:
+        data = {
+            'Name' : name, 
+            'Address' : param[0],
+            'Phone Number' : param[1],
+            'Email Address' : param[2],
+            'Birthday' : param[3]
+        }
+    else: 
+        data = {
+            'Name' : "Does not Exist"
+        }
+    js = json.dumps(data)
+    resp = Response(js, status = 200, mimetype='application/json')
+    return resp
 
 @app.route('/contact')
 def contact(): 
